6e2d960278c78a12156a6dd3b5511256bddda258
Bjørn Tore Svinningen <bt@Bjrns-MBP.lan>
2018-01-25 22:46:44 +0100

    javascript

---
+++ js/01/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	http.Handle("/", http.FileServer(http.Dir(".")))
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
