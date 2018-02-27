92993edbfc330d2cdce0fc107a30cf1d7bd14bc3
Bjørn Tore Svinningen <bt@Bjrns-MBP.lan>
2018-02-27 11:16:15 +0100

    testing go routines and channels

---
+++ channels/01/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ch1 := make(chan int)
+	defer close(ch1)
+
+	go out(ch1)
+	go in(ch1)
+
+	time.Sleep(time.Second * 100)
+}
+
+func out(ch chan int) {
+	for i := 1; i < 100; i++ {
+		ch <- i
+		time.Sleep(time.Second * 1)
+	}
+
+}
+
+func in(ch chan int) {
+	for {
+		msg := <-ch
+		fmt.Println(msg)
+	}
+}
