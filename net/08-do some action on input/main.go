3343fa87ac35b34a65cf9e0d6b850f29284c72fb
Bjørn Tore Svinningen <bt@Bjrns-MBP.lan>
2018-02-27 13:30:41 +0100

    net testing

--- net/08-do some action on input/main.go
+++ net/08-do some action on input/main.go
@@ -1,3 +1,7 @@
+/*
+Starts a tcp listener on localhost port 9000,
+for testing net.Conn, and do something based on the input
+*/
 package main
 
 import (
@@ -28,15 +32,30 @@ func main() {
 func handleConn(conn net.Conn) {
 	defer conn.Close()
 
-	buf := make([]byte, 512)
-
 	for {
+		myC := string("Enter command : ")
+		myCommand := []byte(myC)
+		_, err := conn.Write(myCommand)
+		if err != nil {
+			fmt.Println("error: failed to print command to session")
+		}
+
+		buf := make([]byte, 512)
 		n, err := conn.Read(buf)
 		if err != nil {
 			fmt.Println("error: reading Conn to buf: ", err)
+			break
 		}
 
-		fmt.Println("Reading ", n, "bytes")
+		//convert buffer to string
+		s := string(buf)
+
+		if s[:3] == "ape" {
+			fmt.Println("du skrev ape du !")
+		}
+
+		fmt.Println("Reading ", n, "bytes, which contains the string : ", s)
+
 	}
 
 }
