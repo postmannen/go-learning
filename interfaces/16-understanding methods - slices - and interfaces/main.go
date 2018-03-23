4ab0e30d1b02c04a5d08a8ab4763cdf24adc71a7
Bjørn Tore Svinningen <bt@Bjrns-MBP.lan>
2018-03-23 05:54:39 +0100

    changed the text

--- interfaces/16-understanding methods - slices - and interfaces/main.go
+++ interfaces/16-understanding methods - slices - and interfaces/main.go
@@ -19,15 +19,15 @@ This will create a read method for the tractor type.
 This method could potentially be used to read any kind of information from the tractor,
  and here the info from the tractor is simulated with the string variable engineController.
 The thing here that pulled me off the path to understand this, is that we are actually just passing a
- pre declared slice of bytes of a predefined size as input....but its empty. Since a slice is just a pointer to an
+ pre declared slice of bytes with a predefined size as input....but its empty. Since a slice is just a pointer to an
  underlying array with cap and len we are actually passing something back via the methods input! Since a slice
- is just a pointer to an array, and in this case an array defined within main, the changes to the slice will happen
- dirctly in the slice variable in main.
+ is just a pointer to an array, and in this case an array defined within main, the changes to the slice
+ in the method - will happen directly in the slice variable in main.
  The return part of the method is only used to tell how well the reading went. The return of data is happening via
  the methods input variable.
 */
 func (tractor) read(b []byte) (n int, err error) {
-	engineController := "The engine is running"
+	engineController := "Tractor : The engine is running"
 	copy(b, engineController)
 	return len(engineController), nil
 }
