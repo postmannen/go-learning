13bafe0cbe5060c2386c067e5961436acccc701d
Bjørn Tore Svinningen <bt@Bjrns-MacBook-Pro.local>
2018-04-06 09:18:51 +0200

    adding comments

--- struct/10-using a struct by just referencing the type - also creating a new variable based on referenced type/main.go
+++ struct/10-using a struct by just referencing the type - also creating a new variable based on referenced type/main.go
@@ -8,17 +8,22 @@ type myStruct struct {
 	myString string
 }
 
+//myFuncPrint, just prints out the content of myStructs field myString
 func myFuncPrint(m *myStruct) {
+	//...do something with the struct data given as input
 	fmt.Println(m.myString)
 }
 
+//myFuncPrint, takes a pointer to struct as input, and returns a non-pointer to the same struct type
 func myFuncCreate(m *myStruct) myStruct {
+	//here we dereference the struct, so we're returning a copy of the input struct with all its present values in the struct fields.
 	return *m
 }
 
 func main() {
 	fmt.Println("----------------------------TEST1---------------------------------------")
-	//just call the function with a pointer to  a myStruct type without storing a variable of the type
+	//call the function with a pointer to  a myStruct type without storing a variable of the type,
+	//just directly referencing the struct type with '&', and filling a value for the field of the struct
 	myFuncPrint(&myStruct{myString: "This is a test string"})
 
 	fmt.Println("----------------------------TEST2---------------------------------------")
