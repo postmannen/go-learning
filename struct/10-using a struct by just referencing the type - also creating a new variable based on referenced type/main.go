22744588983a2981903ac1410ac2e4a6722bb926
Bjørn Tore Svinningen <bt@Bjrns-MacBook-Pro.local>
2018-04-06 08:16:24 +0200

    testing structs and using them without creating a variable of the type

---
+++ struct/10-using a struct by just referencing the type - also creating a new variable based on referenced type/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+type myStruct struct {
+	myString string
+}
+
+func myFuncPrint(m *myStruct) {
+	fmt.Println(m.myString)
+}
+
+func myFuncCreate(m *myStruct) myStruct {
+	return *m
+}
+
+func main() {
+	fmt.Println("----------------------------TEST1---------------------------------------")
+	//just call the function with a myStruct type without storing a variable of the type
+	myFuncPrint(&myStruct{myString: "This is a test string"})
+
+	fmt.Println("----------------------------TEST2---------------------------------------")
+	varOfmyStruct := myFuncCreate(&myStruct{myString: "Another test string"})
+	fmt.Printf("The new variable varOfmyStruct is of type = %T, and the myString field contains = %v\n", varOfmyStruct, string(varOfmyStruct.myString))
+
+}
