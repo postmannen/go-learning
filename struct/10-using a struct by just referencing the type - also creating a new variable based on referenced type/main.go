3e0d2f6dbb68a0a2f56ddd468733a530c5a06125
Bjørn Tore Svinningen <bt@Bjrns-MacBook-Pro.local>
2018-04-06 08:19:15 +0200

    testing pointers and structs

--- struct/10-using a struct by just referencing the type - also creating a new variable based on referenced type/main.go
+++ struct/10-using a struct by just referencing the type - also creating a new variable based on referenced type/main.go
@@ -18,10 +18,11 @@ func myFuncCreate(m *myStruct) myStruct {
 
 func main() {
 	fmt.Println("----------------------------TEST1---------------------------------------")
-	//just call the function with a myStruct type without storing a variable of the type
+	//just call the function with a pointer to  a myStruct type without storing a variable of the type
 	myFuncPrint(&myStruct{myString: "This is a test string"})
 
 	fmt.Println("----------------------------TEST2---------------------------------------")
+	//call a function with a pointer to a myStruct type, and return a non-pointer of the same type, and assign it to a variable
 	varOfmyStruct := myFuncCreate(&myStruct{myString: "Another test string"})
 	fmt.Printf("The new variable varOfmyStruct is of type = %T, and the myString field contains = %v\n", varOfmyStruct, string(varOfmyStruct.myString))
 
