b6f0ace1321ec1726e0fd0a4c5f1375ee10173e6
Bjørn Tore Svinningen <bt@Bjrns-MacBook-Pro.local>
2018-04-23 23:55:30 +0200

    replaced msg channel of type string with io.Reader

--- concurrency/10.1-channel training chat/main.go
+++ concurrency/10.1-channel training chat/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"time"
 )
 
 type room struct {
 	ID       int
-	messages chan string      //the active chat messages for all in room
+	messages chan io.Reader   //the active chat messages for all in room
 	clients  map[*client]bool //true if client is in room
 	joining  chan *client
 	leaving  chan *client
@@ -19,9 +21,9 @@ func newRoom(id int) *room {
 	return &room{
 		ID:       id,
 		clients:  make(map[*client]bool),
-		messages: make(chan string),  //initialize channel, or...deadlock
-		joining:  make(chan *client), //initialize channel, or...deadlock
-		leaving:  make(chan *client), //initialize channel, or...deadlock
+		messages: make(chan io.Reader), //initialize channel, or...deadlock
+		joining:  make(chan *client),   //initialize channel, or...deadlock
+		leaving:  make(chan *client),   //initialize channel, or...deadlock
 	}
 }
 
@@ -43,7 +45,7 @@ func (r *room) run() {
 		case c := <-r.joining:
 			r.clients[c] = true
 			//sends message directly to the client, and not in room for all to see
-			c.msg <- "Welcome to the room !"
+			c.msg <- bytes.NewBufferString("Welcome to the room !")
 		case c := <-r.leaving:
 			log.Printf("*** client%v are leaving room%v\n", r.clients[c], r.ID)
 			delete(r.clients, c)
@@ -53,9 +55,9 @@ func (r *room) run() {
 }
 
 type client struct {
-	ID   int         //unique ID of client
-	room *room       //room that client belongs to
-	msg  chan string //the channel to send a message directly to a client
+	ID   int            //unique ID of client
+	room *room          //room that client belongs to
+	msg  chan io.Reader //the channel to send a message directly to a client
 }
 
 //attach a room given as input to the client
@@ -64,7 +66,7 @@ func (c *client) joinRoom(r *room) {
 	//log.Printf("joinRoom: client1.ID =%v, is now in the room client.room.ID = %v\n", c.ID, c.room.ID)
 	//Since Hello message goes to the room all clients will se it
 	myString := fmt.Sprintf("Hello, I'm client%v, and entering the room", c.ID)
-	c.room.messages <- myString
+	c.room.messages <- bytes.NewBufferString(myString)
 
 	//set the client active in the room
 	c.room.joining <- c
@@ -87,7 +89,7 @@ func (c *client) run() {
 func newClient(id int) *client {
 	return &client{
 		ID:  id,
-		msg: make(chan string),
+		msg: make(chan io.Reader),
 	}
 }
 
@@ -103,19 +105,19 @@ func main() {
 	go client1.run()
 
 	//send a test message to the room for all clients to receive
-	room1.messages <- "this is the first message for all clients in the room"
+	room1.messages <- bytes.NewBufferString("this is the first message for all clients in the room")
 
 	client2 := newClient(2)
 	client2.joinRoom(room1)
 	go client2.run()
 
 	//send a test message to the room for all clients to receive
-	room1.messages <- "this is the second message for all clients in the room"
+	room1.messages <- bytes.NewBufferString("this is the second message for all clients in the room")
 
 	client1.leaveRoom(room1)
 
 	//send a test message to the room for all clients to receive
-	room1.messages <- "this is the third message for all clients in the room"
+	room1.messages <- bytes.NewBufferString("this is the third message for all clients in the room")
 
 	time.Sleep(time.Second * 2) //a little delay, will be removed later.
 	fmt.Println("------------------------------------------------------------")
