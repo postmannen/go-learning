7f4acb04fc5c3e88dd806469ff59b333141e4bfe
Bjørn Tore Svinningen <postmannen@gmail.com>
2018-05-08 09:50:21 +0200

    testing

--- div/06-file test/main.go
+++ div/06-file test/main.go
@@ -1,47 +1,62 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"io/ioutil"
-	"net/http"
+	"log"
 	"os"
-	"strings"
 	"time"
 )
 
-type myData struct {
+type aWord struct {
 	word      string
 	timeStamp time.Time
 }
 
-func (m myData) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func readWord(fh *os.File) (aWord string, n int) {
+	myWord := []byte{}
+	var err error
+	for {
+		buf := make([]byte, 1)
+		n, err = fh.Read(buf)
+		if err != nil {
+			log.Println("error: reading file: ", err)
+			break
+		}
 
+		myWord = append(myWord, buf...)
+
+		if buf[0] == ' ' || buf[0] == '\n' {
+			fmt.Println("found space")
+			break
+		}
+	}
+	return string(myWord), n
 }
 
 func main() {
-	//open file
-	fh, err := os.Open("test.txt")
+	//Open a file, use 'test.txt' as default if none specified, or use filename given as input parameter.
+	fileName := flag.String("fileName", "test.txt", "The filename to be checked")
+	//Open file
+	fh, err := os.Open(*fileName)
 	if err != nil {
 		fmt.Println("error: opening file: ", err)
 		os.Exit(1)
 	}
+	defer fh.Close()
 
-	allText, err := ioutil.ReadAll(fh)
-	if err != nil {
-		fmt.Println("error: reading file: ", err)
-	}
-
-	tmpSplit := strings.Split(string(allText), " ")
-
-	words := []myData{}
-	tmpWord := myData{}
-	for _, v := range tmpSplit {
-		tmpWord.word = v
-		tmpWord.timeStamp = time.Now()
-		words = append(words, tmpWord)
+	words := []aWord{}
+	wordFound := aWord{}
+	for {
+		var n int
+		wordFound.word, n = readWord(fh)
+		wordFound.timeStamp = time.Now()
+		words = append(words, wordFound)
+		if n == 0 {
+			break
+		}
 	}
 
 	fmt.Println(words)
-	//http.Handle("/", words)
-	http.ListenAndServe(":8080", nil)
+
 }
