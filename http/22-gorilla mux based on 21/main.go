63e83ab65c17d458e38950e182556c8f8f867cb2
Bjørn Tore Svinningen <postmannen@gmail.com>
2018-06-01 11:00:17 +0200

    added handler funcs for home and contacts

--- http/22-gorilla mux based on 21/main.go
+++ http/22-gorilla mux based on 21/main.go
@@ -3,24 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func myHandleFunc(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "This is the main / ")
-	} else if r.URL.Path == "/contact" {
-		fmt.Fprint(w, "this is the contact page")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintln(w, "the page you requested was not found")
-	}
+	fmt.Fprint(w, "This is the main / ")
+}
 
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "this is the contact page")
 }
 
 func main() {
-	mux := &http.ServeMux{}
+	r := mux.NewRouter()
 
-	mux.HandleFunc("/", myHandleFunc)
-	http.ListenAndServe(":8080", mux)
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	http.ListenAndServe(":8080", r)
 }
