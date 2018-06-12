370a6ecca3e5ccbd74f032f4c5253d8ce059a27c
Bjørn Tore Svinningen <postmannen@gmail.com>
2018-06-12 07:25:17 +0200

    moved template loading to a new newServer function

--- http/25-HandlerFuncs/main.go
+++ http/25-HandlerFuncs/main.go
@@ -28,23 +28,17 @@ type server struct {
 	tmpl     *template.Template
 }
 
-//handleSome is not a real HandleFunc, it is just a normal function who
-// returns a type HandlerFunc.
-//The nice thing with this is that you can prepare and initialise needed to
+//handleSome is not a real HandleFunc, it is just a normal function who returns
+// a type HandlerFunc.
+//The nice thing with this is that you can prepare and initialise whats needed to
 // serve the web page without even calling the web page to be shown if
 // something goes wrong.
 //It is the returned type HandlerFunc thats beeing executed by the HandleFunc function.
 func (s *server) handleSome() http.HandlerFunc {
+	//If we had something that only should be loaded once for a web page we could put it in the s.once.Do
+	// closure below.
 	s.once.Do(func() {
 		fmt.Println("This is only going to be written once !!")
-
-		tmp, err := template.New("myTemplate").Parse(aTemplate)
-		if err != nil {
-			fmt.Println("The template loading or parsing failed", err)
-			panic(err)
-		}
-		s.tmpl = tmp
-
 	})
 
 	//if the loading of the template above was ok, then return the HandlerFunc
@@ -55,11 +49,26 @@ func (s *server) handleSome() http.HandlerFunc {
 	}
 }
 
-func main() {
-	myServer := server{
+func newServer() *server {
+	s := server{
 		name:     "My personal server",
 		hostPort: ":3000",
 	}
+
+	//putting the template loading in the newServer rather than in the handler or main, since
+	// it feels like its belonging in creating a new server, and should only be loaded once.
+	tmp, err := template.New("myTemplate").Parse(aTemplate)
+	if err != nil {
+		fmt.Println("The template loading or parsing failed", err)
+		panic(err)
+	}
+	s.tmpl = tmp
+
+	return &s
+}
+
+func main() {
+	myServer := newServer()
 
 	http.HandleFunc("/some", myServer.handleSome())
 	http.ListenAndServe(myServer.hostPort, nil)
