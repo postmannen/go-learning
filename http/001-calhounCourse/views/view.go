4135ac9f10ddb4f29d633b82f0296fd593cbd12d
Bjørn Tore Svinningen <postmannen@gmail.com>
2018-06-20 07:30:34 +0200

    refactored template handling

--- http/001-calhounCourse/views/view.go
+++ http/001-calhounCourse/views/view.go
@@ -1,16 +1,20 @@
 package views
 
 import (
+	"fmt"
 	"html/template"
+	"path/filepath"
 )
+
+//LayoutDir the directory where all the layouts are, defaults to "views/layouts/""
+var LayoutDir = "views/layouts/"
+
+//TemplateExt defaults to ".html"
+var TemplateExt = ".html"
 
 //NewView used to create new view with templates attached
 func NewView(layout string, files ...string) *View {
-	files = append(files,
-		"views/layouts/bootstrap.html",
-		"views/layouts/navbar.html",
-		"views/layouts/footer.html",
-	)
+	files = append(files, layoutFiles()...)
 	t, err := template.ParseFiles(files...)
 	if err != nil {
 		panic(err)
@@ -26,4 +30,13 @@ func NewView(layout string, files ...string) *View {
 type View struct {
 	Template *template.Template
 	Layout   string
+}
+
+//layoutFiles will get a slice containing the name of all the *.html files in the views/layout directory
+func layoutFiles() []string {
+	files, err := filepath.Glob(LayoutDir + "*" + TemplateExt)
+	if err != nil {
+		fmt.Println("Error: filepath.Glob failed :", err)
+	}
+	return files
 }
