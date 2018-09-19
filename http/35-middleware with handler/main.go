172466b0f2042c4fee93556dab027aa423e91d17
Bjørn Tore Svinningen <postmannen@gmail.com>
2018-09-19 12:56:05 +0200

    testing

--- http/35-middleware with handler/main.go
+++ http/35-middleware with handler/main.go
@@ -6,17 +6,19 @@ import (
 	"net/http"
 )
 
+//Test1-------------------------------------------------
 type myType struct {
 }
 
+//By giving myType a method named ServeHTTP(w,r) it satisfies
+//the Handler interface, and then also becomes a type Handler.
 func (m myType) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "The root Handle")
+	fmt.Fprintln(w, "The root Handle writes this")
 }
 
 func logger(next http.Handler) http.Handler {
-	//Cast a Handler into a HandlerFunc
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "This is the logger wrapper")
+		fmt.Fprintln(w, "The logger wrapper writes this")
 		next.ServeHTTP(w, r)
 	})
 }
@@ -25,5 +27,6 @@ func main() {
 	a := myType{}
 
 	http.Handle("/", logger(a))
+
 	http.ListenAndServe(":8080", nil)
 }
