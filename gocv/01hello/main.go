060998638cbfaac94ba28add11eb2ec88fdc7bdd
postmannen@gmail.com <postmannen@gmail.com>
2018-10-01 17:10:53 +0200

    testing openCV

---
+++ gocv/01hello/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"gocv.io/x/gocv"
+)
+
+func main() {
+	webcam, _ := gocv.VideoCaptureDevice(0)
+	window := gocv.NewWindow("Hello")
+	img := gocv.NewMat()
+
+	for {
+		webcam.Read(&img)
+		window.IMShow(img)
+		window.WaitKey(1)
+	}
+}
