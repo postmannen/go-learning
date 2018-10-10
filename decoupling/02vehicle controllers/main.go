4b2109991fa43aeffc07ff8ef45be13706f3afc7
Bjørn Tore Svinningen <postmannen@gmail.com>
2018-10-10 11:23:09 +0200

    implemented Reader interface

--- decoupling/02vehicle controllers/main.go
+++ decoupling/02vehicle controllers/main.go
@@ -6,6 +6,8 @@ import (
 	"math/rand"
 )
 
+// ==========================================================
+
 //tractorBus simulates a bus on a tractor controller
 type tractorBus struct {
 	bus chan int
@@ -35,17 +37,60 @@ func (t *tractorBus) Read() (int, error) {
 }
 
 // ==========================================================
+//carBus simulates a bus on a tractor controller
+type carBus struct {
+	bus chan int
+}
+
+func newCarBus() *carBus {
+	cb := make(chan int)
+	return &carBus{
+		bus: cb,
+	}
+}
+
+//Start will make the tractor bus producing values on the channel
+func (c *carBus) Start() {
+	go func() {
+		for {
+			c.bus <- rand.Intn(10)
+		}
+	}()
+
+}
+
+//Read will read 1 value from the channel
+func (c *carBus) Read() (int, error) {
+	d := <-c.bus
+	return d, nil
+}
 
 // ==========================================================
 
-func main() {
-	tractor := newTractorBus()
-	tractor.Start()
-	d, err := tractor.Read()
+//Reader is a reader interface type for all vehicles
+type Reader interface {
+	Read() (int, error)
+}
+
+// ==========================================================
+
+func readToScreen(v Reader, amount int) error {
+	tractorData, err := v.Read()
 	if err != nil {
 		log.Println("Error: Read tractor: ", err)
 	}
 
-	fmt.Println("Read returned : ", d)
+	fmt.Println("Read tractor returned : ", tractorData)
+	return nil
+}
+
+func main() {
+	tractor := newTractorBus()
+	tractor.Start()
+	readToScreen(tractor, 5)
+
+	car := newCarBus()
+	car.Start()
+	readToScreen(car, 5)
 
 }
