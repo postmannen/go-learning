c448eb5411192926709064d1e610894ac50be7fc
Bjørn Tore Svinningen <postmannen@gmail.com>
2018-10-30 17:38:37 +0100

    testing

--- xml/02/main.go
+++ xml/02/main.go
@@ -33,11 +33,20 @@ func main() {
 		//Remove leading spaces in line
 		tmpLine := strings.TrimSpace(string(line))
 		line = []byte(tmpLine)
-		printLine(line)
+		//printLine(line)
 
-		err = checkAngleBracket(line)
-		if err != nil {
-			fmt.Println(err)
+		if tp := checkTagProject(line); tp.foundStart {
+			fmt.Println("AAA found project start on lineNR : ", lineNR)
+			if err := checkForClosingBracket(line); err != nil {
+				log.Fatal("Error: missed closing bracket")
+			}
+		}
+
+		if tp := checkTagProject(line); tp.foundEnd {
+			fmt.Println("AAA found project end on lineNR : ", lineNR)
+			if err := checkForClosingBracket(line); err != nil {
+				log.Fatal("Error: missed closing bracket")
+			}
 		}
 
 		lineNR++
@@ -55,7 +64,73 @@ func printLine(line []byte) {
 	fmt.Println()
 }
 
-func checkAngleBracket(line []byte) error {
+type tagProject struct {
+	foundStart bool
+	foundEnd   bool
+	name       string
+	id         string
+}
+
+func checkTagProject(line []byte) tagProject {
+	var tag string
+	if len(line) > 0 {
+		tag = string(line[0:8])
+		if tag == "<project" {
+			tp := tagProject{foundStart: true}
+
+			//find word in []byte
+			myWordString := "name="
+			myWordByte := []byte(myWordString)
+			foundWord := false
+
+			for linePosition := 0; linePosition < len(line)-len(myWordByte); linePosition++ {
+				wordPosition := 0
+				for {
+					if wordPosition >= len(myWordByte) {
+						fmt.Println("Reached the end of the word, breaking out of word loop", linePosition, wordPosition)
+						foundWord = true
+						break
+					}
+
+					fmt.Println("Comparing : ", line[linePosition+wordPosition], myWordByte[wordPosition])
+
+					if line[linePosition+wordPosition] == myWordByte[wordPosition] {
+						fmt.Println("found letter: ", string(myWordByte[wordPosition]))
+					} else {
+						fmt.Println("Letters don't match, breaking out of inner loop")
+						break
+					}
+
+					//if wordPosition == len(myWordByte)-1 {
+					//	fmt.Println("Found the word")
+					//	break
+					//}
+
+					wordPosition++
+				}
+
+				if foundWord {
+					fmt.Println("Breaking out of outer loop")
+					break
+				}
+			}
+
+			return tp
+		}
+
+		tag = string(line[0:9])
+		if tag == "</project" {
+			tp := tagProject{foundEnd: true}
+			return tp
+		}
+
+	}
+
+	tp := tagProject{}
+	return tp
+}
+
+func checkForClosingBracket(line []byte) error {
 	//Check for opening and closing angle bracket.
 	//Will return nil if both start and end bracker was found.
 	for i := 0; i < len(line); i++ {
@@ -68,8 +143,8 @@ func checkAngleBracket(line []byte) error {
 					break
 				}
 				if ii == len(line)-1 {
-					fmt.Println("Missing ending angular bracket at line: ")
-					return errors.New("Missing ending angular bracket at line: ")
+
+					return errors.New("Missing ending angular bracket")
 				}
 				ii++
 			}
