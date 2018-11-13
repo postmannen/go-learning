3f19b422cf9146f939235a50b129a1044e40a301
Bjørn Tore Svinningen <postmannen@gmail.com>
2018-11-13 21:00:04 +0100

    testing

--- function/13-statemachine - NOT DONE - LEARNING/main.go
+++ function/13-statemachine - NOT DONE - LEARNING/main.go
@@ -10,19 +10,29 @@ type data struct {
 	numbers []int
 }
 
+func (d *data) less() aFunction {
+	fmt.Println("less : ", d.counter, " is less than 5")
+	return d.someFunction
+}
+
+func (d *data) more() aFunction {
+	fmt.Println("more : ", d.counter, " is more than 5")
+	return d.someFunction
+}
+
 func (d *data) someFunction() aFunction {
 	d.counter++
 	if d.counter == d.length {
 		return nil
 	}
 
-	if d.numbers[d.counter] < 5 {
-		fmt.Println(d.counter, " is less than 5")
-		return d.someFunction
+	if d.numbers[d.counter] <= 5 {
+		fmt.Println("someFunction : ", d.counter, " is less than 5")
+		return d.less
 	}
 	if d.numbers[d.counter] > 5 {
-		fmt.Println(d.counter, " is larger than 5")
-		return d.someFunction
+		fmt.Println("someFunction : ", d.counter, " is larger than 5")
+		return d.more
 	}
 
 	return nil
@@ -31,10 +41,16 @@ func (d *data) someFunction() aFunction {
 func main() {
 	d := &data{
 		counter: 0,
-		numbers: []int{0, 1, 2, 4, 5, 6, 7, 8, 9},
+		numbers: []int{0, 1, 2, 4, 5, 6, 7, 8, 9, 10, 11},
 	}
 	d.length = len(d.numbers)
+	for {
+		f := d.someFunction()
+		fmt.Println("DEBUG : in for loop d.counter = ", d.counter)
+		if f == nil {
+			break
+		}
 
-	d.someFunction()
+	}
 
 }
