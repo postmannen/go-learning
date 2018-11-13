150857c58b18cf835d5a00118cf62cfb0e556670
Bjørn Tore Svinningen <postmannen@gmail.com>
2018-11-14 00:03:04 +0100

    added comments

--- function/13-statemachine - NOT DONE - LEARNING/main.go
+++ function/13-statemachine - NOT DONE - LEARNING/main.go
@@ -1,3 +1,10 @@
+//The idea here is to test out having a function returning
+// another function to be executed next.
+// The reason we might want to do something like this can
+// be that we are iterating a slice, and if we want to do
+// something special depending on the value found, we return
+// the next function to be called. That function again can
+// call another one...and so on.
 package main
 
 import "fmt"
@@ -8,30 +15,39 @@ type data struct {
 	counter int
 	length  int
 	numbers []int
+	aFunction
 }
 
 func (d *data) less() aFunction {
 	fmt.Println("less : ", d.counter, " is less than 5")
-	return d.someFunction
+	return d.checkSize
 }
 
 func (d *data) more() aFunction {
 	fmt.Println("more : ", d.counter, " is more than 5")
-	return d.someFunction
+	return d.checkSize
 }
 
-func (d *data) someFunction() aFunction {
+func (d *data) checkSize() aFunction {
 	d.counter++
+	// If we are at the end of the slice, return nil back to main,
+	// and break the for loop there. Since nil contains no function
+	// to call no more functions will be called.
 	if d.counter == d.length {
 		return nil
 	}
 
+	// If the number is below 5, return the d.less function to be
+	// executed in main for loop.
 	if d.numbers[d.counter] <= 5 {
-		fmt.Println("someFunction : ", d.counter, " is less than 5")
+		fmt.Println("checkSize : ", d.counter, " is less than 5")
 		return d.less
 	}
+
+	// If the number is above 5, return the d.more function to be
+	// executed in main for loop.
 	if d.numbers[d.counter] > 5 {
-		fmt.Println("someFunction : ", d.counter, " is larger than 5")
+		fmt.Println("checkSize : ", d.counter, " is larger than 5")
 		return d.more
 	}
 
@@ -44,9 +60,20 @@ func main() {
 		numbers: []int{0, 1, 2, 4, 5, 6, 7, 8, 9, 10, 11},
 	}
 	d.length = len(d.numbers)
+
+	// Here we are starting the first function who will start it all,
+	// and put the returned function into f.
+	f := d.checkSize()
 	for {
-		f := d.someFunction()
-		fmt.Println("DEBUG : in for loop d.counter = ", d.counter)
+		fmt.Println("------------------------------------------------------------------")
+		fmt.Printf("Main: Calling %#v\n", f)
+		// Here we are constantly looping and executing the returned functions.
+		// Again we put the returned function value into f to then be executed
+		// on the next round of the for loop.
+		// The d.checkSize function will check the if we have reached the end of
+		// the slice, and return nil instead of another function to call so we
+		// can exit the for loop here by checking for nil, and then break the loop.
+		f = f()
 		if f == nil {
 			break
 		}
