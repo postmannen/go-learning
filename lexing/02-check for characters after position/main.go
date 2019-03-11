a054e4aa1feeeb77bb800ab4c79632a8bd124d6b
Bjørn Tore Svinningen <postmannen@gmail.com>
2019-03-11 20:43:53 +0100

    added comments

--- lexing/02-check for characters after position/main.go
+++ lexing/02-check for characters after position/main.go
@@ -27,6 +27,10 @@ func main() {
 
 //checkForChrAfter takes a string, a position in a string, and a string pattern to look for as input.
 // Returns true if the character combination was found after the given position.
+// The function will first look for the first character in the given string,
+// and if that character is found we get into the inner for loop and loop over the
+// character pattern to check if the rest of the characters match.
+// If match we return, the value true, and exit the function.
 func checkForChrAfter(s string, curPos int, characters string) (found bool) {
 	for i := curPos; i < len(s)-1; i++ {
 		if s[i] == characters[0] {
