2a199de42ddb56160dcee991a7ce2e08298b783d
Bjørn Tore Svinningen <postmannen@gmail.com>
2019-03-20 07:50:31 +0100

    optimizing for benchmarking

--- binary/04-convert-slice-of-bools-to-uint8/main.go
+++ binary/04-convert-slice-of-bools-to-uint8/main.go
@@ -15,20 +15,25 @@ func main() {
 
 func convert(b []bool) uint8 {
 	var n uint8
-	for i, v := range b {
+	for i := 0; i < len(b)-1; i++ {
 		//fmt.Printf("loop, iterating i=%v, value=%v, type=%T\n", i, v, v)
 
 		//If true we swap the least significant bit to 1,
 		// if false we leave it to its default which is 0.
-		if v == true {
+		if b[i] == true {
 			n = n | 1
 		}
 
-		//Bit shift all 1 to the left, which means..we add a zero to the end.
-		if i < len(b)-1 {
-			n = n << 1
-		}
+		//Bit shift all 1 to the left, which means..we add a zero to the end
+		// to make room for the next value.
+		n = n << 1
 		//fmt.Printf("Value is now, %b\n", n)
+	}
+
+	//instead of checking for the end after each iteration to handle the last bit,
+	// we do it after, and go from 14.2 ns/op to 12.0 ns/op
+	if b[len(b)-1] == true {
+		n = n | 1
 	}
 
 	return n
