e1206bb251536ab166f1113f8fcd5340c9304eb0
Bjørn Tore Svinningen <postmannen@gmail.com>
2019-05-14 09:50:05 +0200

    comments

--- net/rpc/01-rpcTest/server/main.go
+++ net/rpc/01-rpcTest/server/main.go
@@ -1,3 +1,7 @@
+/*
+Test for learning RPC.
+RPC uses GOB as the binary format by default when sending the data over the network.
+*/
 package main
 
 import (
@@ -13,8 +17,16 @@ type Person struct {
 	Name string
 }
 
+//PersonRPC is a type to tie the RPC exposed methods to.
+// Not sure if it is correct to make it's own type for the RPC exposed methods, or if they should be
+// placed on the data structure Person instead. Have to investigate further on that in another example.
 type PersonRPC int
 
+//Print
+// An RPC method needs to meet to accept two arguments, and return an error.
+// Argument 1, should be the data to receive
+// Argument 2, will be the data to return back, in this example a reply code in the form of "ok".
+// If the method call fails, an error will be returned, and the argument 2 will be set to zero/0.
 func (m PersonRPC) Print(v Person, status *string) error {
 	fmt.Println("** Printing : ", v.Name)
 	*status = "ok"
