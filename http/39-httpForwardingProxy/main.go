ef2d7a602f8d06dd71b5eca40df67951d0ae8ea8
Bjørn Tore Svinningen <postmannen@gmail.com>
2019-05-15 22:58:06 +0200

    testing proxy

---
+++ http/39-httpForwardingProxy/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func pageHandler(w http.ResponseWriter, r *http.Request) {
+	//fmt.Fprintf(w, "%#v\n", r)
+	resp, err := http.Get("http://vg.no")
+	if err != nil {
+		log.Println("error: get: ", err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("*****************************1")
+
+	//The header is a map, we take all the pieces from the received header, and put the same
+	// key's and values into our own header.
+	for name, values := range resp.Header {
+		w.Header()[name] = values
+	}
+
+	w.WriteHeader(resp.StatusCode)
+
+	//test to print some on top of the received body.
+	fmt.Fprint(w, "apekatt")
+
+	n, err := io.Copy(w, resp.Body)
+	fmt.Println("********Copied bytes = ", n)
+	if err != nil {
+		log.Println("error: copy: ", err)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", pageHandler)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Println("listen and serve : ", err)
+	}
+
+}
