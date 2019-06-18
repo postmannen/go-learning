fa44e7b5692c35b7af2fb7e53d20f93319c6be5e
Bjørn Tore Svinningen <postmannen@gmail.com>
2019-06-18 06:50:01 +0200

    json

--- json/06-marshallingAnonymousStruct/main.go
+++ json/06-marshallingAnonymousStruct/main.go
@@ -1,6 +1,6 @@
 package main
 
-import "github.com/postmannen/go-learning/json/06/somepkg"
+import "github.com/postmannen/go-learning/json/06-marshallingAnonymousStruct/somepkg"
 
 func main() {
 	somepkg.MarshallSome()
--- json/06/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/postmannen/go-learning/json/06-marshallingAnonymousStruct/somepkg"
-
-func main() {
-	somepkg.MarshallSome()
-}
