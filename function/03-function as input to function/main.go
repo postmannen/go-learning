26f9c755cd04efc8b7fd84a03de2c7583da0d9b6
Bjørn Tore Svinningen <postmannen@gmail.com>
2019-08-01 16:35:13 +0200

    added comments

--- function/03-function as input to function/main.go
+++ function/03-function as input to function/main.go
@@ -9,8 +9,13 @@ func addToString(s string) string {
 
 }
 
-//let the function accept another function as a parameter
+// Let the function accept another function as a parameter
+// As one of the parameters we accept a function with the
+// signature 'func(string) string', which means we can use any
+// function that takes a string as input, and returns a string.
 func stringMe(name string, addS func(string) string) string {
+	// The function will be executed when returning from this
+	// function.
 	return addS(name)
 }
 
