af5ca4bf1b252c87531d28a251d1282bb309e993
Bjørn Tore Svinningen <postmannen@gmail.com>
2019-08-01 16:31:18 +0200

    updated named return variables

--- function/02-namedReturnValues/main.go
+++ function/02-namedReturnValues/main.go
@@ -8,7 +8,11 @@ func main() {
 	fmt.Println(sum1, sum2)
 }
 
-//calculate adds and multiplicates two variables, returns the add sum and multiplication sum
+// calculate adds and multiplicates two variables, returns the add sum and multiplication sum
+// We can add names to the return variables and make it document how the function works in a
+// better way. We don't even have to use the variables the function declaration creates,
+// we can just return our own ones used in the function body as long as they are of the same
+// type.
 func calulate(x int, y int) (sumAdded int, sumMultiplicated int) {
 	add := x + y
 	mul := x * y
