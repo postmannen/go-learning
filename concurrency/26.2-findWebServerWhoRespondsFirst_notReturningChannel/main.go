ad1d622f01c59637dd8b11b43efa7c1034574f77
Bjørn Tore Svinningen <postmannen@gmail.com>
2019-09-26 19:49:14 +0200

    removed returning of channel from previous example

---
+++ concurrency/26.2-findWebServerWhoRespondsFirst_notReturningChannel/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+	"time"
+)
+
+// findFastestWeb will take a slice of url's represented as a []string,
+// and return back the response of the webserver that returned the
+// complete page first as respData
+func findFastestWeb(url []string) respData {
+	var found bool
+	var mu sync.Mutex // mutex to protect the first to update the found variable.
+	ch := make(chan respData, 1)
+
+	// Loop over all the url's given as input, and start a go routine
+	// with a http.Get for each of them.
+	for _, u := range url {
+		go func(u string) {
+			t := time.Now()
+			resp, err := http.Get(u)
+			if err != nil {
+				log.Println("error: http.Get for one", err)
+			}
+
+			if !found {
+				mu.Lock()
+				found = true
+				mu.Unlock()
+
+				totalTime := time.Until(t)
+
+				ch <- respData{time: totalTime, resp: resp}
+				return
+			}
+		}(u)
+
+	}
+	r := <-ch
+	return r
+}
+
+type respData struct {
+	time time.Duration
+	resp *http.Response
+}
+
+func main() {
+
+	urls := []string{"https://dagbladet.no", "https://aftenposten.no", "https://vg.no"}
+
+	r := findFastestWeb(urls)
+	defer r.resp.Body.Close()
+
+	fmt.Printf("%v : finalized first, and in time it took %v\n", r.resp.Request.URL, r.time)
+}
