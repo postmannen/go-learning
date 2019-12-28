e8bbd39e38b9c69293d1917ebdc173b2e25ce199
Bjørn Tore Svinningen <postmannen@gmail.com>
2019-12-28 10:21:42 +0100

    testing

---
+++ grpc/01-greet-toBeFinished/cmd/server/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "net"
+
+import "fmt"
+
+//import "google.golang.org/grpc"
+
+func main() {
+	//l, err := net.Listen("tcp", "localhost:50051")
+	//if err != nil {
+	//	fmt.Printf("error: failed to open listener: %v\n", err)
+	//}
+
+	
+
+}
