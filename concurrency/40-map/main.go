cf381f6f5ba86f968122b6c97078f48715d9e893
postmannen <postmannen@gmail.com>
2021-10-06 17:35:07 +0200

    concurrent map read

--- concurrency/40-map/main.go
+++ concurrency/40-map/main.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"runtime"
 	"strconv"
 	"sync"
+	"time"
 )
 
 type keyValue struct {
@@ -22,18 +24,20 @@ type getValue struct {
 }
 
 type cMap struct {
-	m      map[string]string
-	mInCh  chan kvCh
-	mGetCh chan getValue
-	mDelCh chan kvCh
+	m         map[string]string
+	mInCh     chan kvCh
+	mGetCh    chan getValue
+	mDelCh    chan kvCh
+	mGetAllCh chan chan []keyValue
 }
 
 func newCMap() *cMap {
 	cM := cMap{
-		m:      map[string]string{},
-		mInCh:  make(chan kvCh),
-		mGetCh: make(chan getValue),
-		mDelCh: make(chan kvCh),
+		m:         map[string]string{},
+		mInCh:     make(chan kvCh),
+		mGetCh:    make(chan getValue),
+		mDelCh:    make(chan kvCh),
+		mGetAllCh: make(chan chan []keyValue),
 	}
 	return &cM
 }
@@ -53,6 +57,16 @@ func (c *cMap) run(ctx context.Context) {
 			kv := <-kvCh
 			delete(c.m, kv.k)
 
+		case gaCh := <-c.mGetAllCh:
+			kvSlice := []keyValue{}
+
+			for k, v := range c.m {
+				kv := keyValue{k: k, v: v}
+				kvSlice = append(kvSlice, kv)
+			}
+
+			gaCh <- kvSlice
+
 		case <-ctx.Done():
 			log.Printf("info: cMap: got ctx.Done\n")
 			return
@@ -64,7 +78,6 @@ func (c *cMap) put(kv keyValue) {
 	kvCh := make(chan keyValue, 1)
 	kvCh <- kv
 	c.mInCh <- kvCh
-
 }
 
 func (c *cMap) get(key string) keyValue {
@@ -87,7 +100,17 @@ func (c *cMap) del(kv keyValue) {
 
 }
 
+func (c *cMap) getAll() []keyValue {
+	gaCh := make(chan []keyValue, 1)
+	c.mGetAllCh <- gaCh
+
+	all := <-gaCh
+	return all
+}
+
 func main() {
+	runtime.GOMAXPROCS(1)
+
 	var wg sync.WaitGroup
 	cM := newCMap()
 	ctx, cancel := context.WithCancel(context.Background())
@@ -102,12 +125,15 @@ func main() {
 	{
 		var wg sync.WaitGroup
 		const times = 3
+		const keysValues = 20
+
+		fmt.Println("-----------------------concurrently put and get--------------------------------------")
 
 		for i := 0; i < times; i++ {
 			// Fill values
 			wg.Add(1)
 			go func() {
-				for ii := 0; ii < 100; ii++ {
+				for ii := 0; ii < keysValues; ii++ {
 					s := strconv.Itoa(ii)
 					sTen := strconv.Itoa(ii * 10)
 					cM.put(keyValue{k: s, v: sTen})
@@ -117,14 +143,15 @@ func main() {
 
 			wg.Add(1)
 			go func() {
-				kv := cM.get("3")
+				defer wg.Done()
+				time.Sleep(time.Millisecond * 100)
+				kv := cM.get("5")
 				if !kv.ok {
 					fmt.Printf("info: no value found when key = %v\n", kv.k)
 					return
 				}
 
 				fmt.Printf("key: %v, value: %v\n", kv.k, kv.v)
-				wg.Done()
 			}()
 		}
 
@@ -133,14 +160,24 @@ func main() {
 
 	{
 		// Deletion of key
-		cM.del(keyValue{k: "50"})
-		kv := cM.get("50")
+		fmt.Println("-----------------------key deletion--------------------------------------")
+		fmt.Println("deleting key")
+		cM.del(keyValue{k: "5"})
+		kv := cM.get("5")
 		if !kv.ok {
-			fmt.Printf("info: no value found when key = %v\n", kv.k)
-			return
+			fmt.Printf("info: delete, no value found when key = %v\n", kv.k)
+		} else {
+			fmt.Printf("key: %v, value: %v\n", kv.k, kv.v)
 		}
 
-		fmt.Printf("key: %v, value: %v\n", kv.k, kv.v)
+	}
+
+	{
+		// Get all key/values
+		fmt.Println("-----------------------get all values--------------------------------------")
+		kvAll := cM.getAll()
+		fmt.Printf("Len of slice with getAll : %v\n", len(kvAll))
+		fmt.Printf("All values returned as a slice:\n %#v\n", kvAll)
 	}
 
 	cancel()
