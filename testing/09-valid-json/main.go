0f1d8d98058e61c9cab6352b88989c418ed1c350
postmannen <postmannen@gmail.com>
2022-05-25 14:43:22 +0200

    test

--- testing/09-valid-json/main.go
+++ testing/09-valid-json/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -10,25 +11,47 @@ import (
 )
 
 func main() {
+
 	jsonFilepaths := getJsonFilePaths()
 	fmt.Printf("file=%v, and the type is %T\n", jsonFilepaths, jsonFilepaths)
 
+	var failure int
+
 	for _, f := range jsonFilepaths {
-		fh, err := os.Open(f)
-		if err != nil {
-			log.Printf("error: failed to open file for reading: %v\n", err)
-			os.Exit(1)
-		}
-		// Remember to close the file!
+		func() {
+			fh, err := os.Open(f)
+			if err != nil {
+				log.Printf("error: failed to open file for reading: %v\n", err)
+				os.Exit(1)
+			}
+			defer fh.Close()
 
-		js, err := io.ReadAll(fh)
-		if err != nil {
-			log.Printf("error: failed to read file: %v\n", err)
-			os.Exit(1)
-		}
+			js, err := io.ReadAll(fh)
+			if err != nil {
+				log.Printf("error: failed to read file: %v\n", err)
+				os.Exit(1)
+			}
 
-		fmt.Printf(" * Content of file %v : %v\n", f, string(js))
+			fmt.Printf(" * Content of file %v : %v\n", f, string(js))
 
+			var data map[string]any
+
+			if len(js) < 1 {
+				fmt.Printf(" * the file %v was empty: ", f)
+				return
+			}
+
+			err = json.Unmarshal(js, &data)
+			if err != nil {
+				log.Printf("error: for file %v, failed to unmarshal data: %v\n", f, err)
+				failure = 1
+			}
+		}()
+
+	}
+
+	if failure != 0 {
+		os.Exit(failure)
 	}
 
 }
